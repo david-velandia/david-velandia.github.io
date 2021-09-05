d5f19479c007d5bcebdc36af8312ca8a08b74902
david-velandia <80493730+david-velandia@users.noreply.github.com>
2021-09-05 02:22:49 +0000

    Deploying to gh-pages from @ david-velandia/david-velandia.github.io@3c50107d5164f7c7304701d086a012cb507b5701 🚀

---
+++ _next/static/n9KHIAhfnsrGiBeMP3hqW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-66ec6ac012aafa1964d7.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
